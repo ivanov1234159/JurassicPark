3ce4c90f5607625cfe6709003a126b89b6730cef
Veselin Ivanov <ivanov1234159@abv.bg>
2020-04-21 20:18:56 +0300

    create add (dinosaur) command; fix main() in main.cpp to use class Commander

--- build_commands.cpp
+++ build_commands.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "Commander.hpp"
+#include "Cage.hpp"
+#include "Dinosaur.hpp"
 
 /* For all of the following functions:
  *      returns false if there is a problem with the arguments (their type or count)
@@ -60,9 +62,52 @@ bool cmd_create_cage(RunnerType& runner, std::istringstream& iss){
     return true;
 }
 
+bool cmd_add_animal(RunnerType& runner, std::istringstream& iss){
+    char buff[Commander::BUFFER_SIZE];
+    iss >> buff;
+    if(!iss){
+        std::cout << "Some error. Cannot read the name." << std::endl;//TODO: fix message
+        return false;
+    }
+    Dinosaur dino;
+    dino.setName(buff);
+    iss >> buff;
+    if(!iss || !dino.setGender(buff)){
+        std::cout << "Invalid gender." << std::endl;
+        return false;
+    }
+    iss >> buff;
+    if(!iss || !dino.setEra(buff)){
+        std::cout << "Invalid era." << std::endl;
+        return false;
+    }
+    iss >> buff;
+    if(!iss || !dino.setType(buff)){
+        std::cout << "Invalid type." << std::endl;
+        return false;
+    }
+    iss >> buff;
+    if(!iss || !dino.setSpecies(buff)){
+        std::cout << "Invalid species." << std::endl;
+        return false;
+    }
+    iss >> buff;
+    if(!iss || !dino.setFood(buff)){
+        std::cout << "Invalid food name." << std::endl;
+        return false;
+    }
+    if(!runner.addAnimal(dino)){
+        std::cout << "No cage for this dinosaur. Please create one." << std::endl;
+    } else {
+        std::cout << "The dinosaur added successfully." << std::endl;
+    }
+    return true;
+}
+
 void build_commands(){
-    Commander::add(Command("exit", "", "exit from the program", cmd_exit));
+    Commander::add(Command("exit", "", "exit from the program\nbut before that do the 'save' command", cmd_exit));
     Commander::add(Command("save", "", "save the current state of the program", cmd_save));
     Commander::add(Command("help", "", "prints this information", cmd_help));
     Commander::add(Command("create", "<climate> <size>", "create a cage with given <climate> and <size>\n<size> can be 'small' (for 1), 'medium' (for 3)\nor 'large' (for 10)", cmd_create_cage));
+    Commander::add(Command("add", "<name> <gender> <era> <type> <species> <food>", "add a new dinosaur in the zoopark", cmd_add_animal));
 }
