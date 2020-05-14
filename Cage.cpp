267563f38e2593c091614a33da9b435b6d6a9e60
Veselin Ivanov <ivanov1234159@gmail.com>
2020-05-14 17:28:05 +0300

    reorganize class Cage to use classes String and Vector<T>

--- Cage.cpp
+++ Cage.cpp
@@ -2,111 +2,77 @@
 // Created by toi on 25/03/2020.
 //
 
-#include <cstring>
-//for: strcmp()
 #include "Cage.hpp"
 #include "MySpace.hpp"
 
-unsigned Cage::getCageSize(char const* size) {
-    if(size != nullptr){
-        if(std::strcmp(size, "small") == 0){
-            return 1;
-        }else if(std::strcmp(size, "medium") == 0){
-            return 3;
-        }else if(std::strcmp(size, "large") == 0){
-            return 10;
-        }
+unsigned Cage::getCageSize(String const& size) {
+    if(size == "small"){
+        return 1;
+    }else if(size == "medium"){
+        return 3;
+    }else if(size == "large"){
+        return 10;
     }
     return Cage::DEFAULT_CAGE_SIZE;
 }
 
-Cage::Cage(char const* climate, char const* size): m_climate(nullptr), m_era(nullptr), m_list(nullptr) {
-    copy(climate, Cage::getCageSize(size), nullptr, nullptr, 0);
-}
-
-Cage::Cage(const Cage& other): m_climate(nullptr), m_era(nullptr), m_list(nullptr) {
-    copy(other);
-}
-
-Cage& Cage::operator=(const Cage& other){
-    if(this != &other){
-        //clear();
-        copy(other);
+Cage::Cage(String const& climate, String const& size): m_climate(climate) {
+    unsigned capacity = Cage::getCageSize(size);
+    if(capacity != Cage::DEFAULT_CAGE_SIZE){
+        m_list = Vector<Dinosaur>(capacity, true);
     }
-    return *this;
 }
-
-Cage::~Cage(){
-    clear();
-}
-
-bool Cage::empty() const {
-    return m_list_count == 0;
-}
-
-bool Cage::full() const {
-    return m_list_count == m_size;
-}
-
-unsigned Cage::free() const {
-    return m_size - m_list_count;
-}
-
 
 bool Cage::canHandle(const Dinosaur& dinosaur){
-    if(full() || (m_era != nullptr && std::strcmp(m_era, dinosaur.getEra()) != 0)){
-        return false;
-    }
-    if(empty()){
+    if(m_list.empty()){
         return true;
     }
-    return std::strcmp(m_list[0].getType(), dinosaur.getType()) != 0;
+    if(m_list.full() || dinosaur.getEra() != m_era){
+        return false;
+    }
+    return m_list[0].getType() == dinosaur.getType();
 }
 
 bool Cage::add(const Dinosaur& dinosaur){
     if(!canHandle(dinosaur)){
         return false;
     }
-    if(empty()){
-        MySpace::mem_copy(m_era, dinosaur.getEra());
+    if(m_list.empty()){
+        m_era = dinosaur.getEra();
     }
-    m_list[m_list_count] = dinosaur;
-    m_list_count++;
-    return true;
+    return m_list.push(dinosaur);
 }
 
 bool Cage::remove(const char* dinosaur_name){
-    Dinosaur* search = find(dinosaur_name);
-    if(search == nullptr){
+    int search = findIndex(dinosaur_name);
+    if(search < 0){
         return false;
     }
-    Dinosaur* last = &m_list[m_list_count-1];
-    while (search < last){
-        *search = *(search + 1);
-        search++;
-    }
+    m_list.remove(search);
     return true;
 }
 
-Dinosaur* Cage::find(const char* dinosaur_name) const {
-    for(unsigned i = 0; i < m_list_count; i++){
-        if(std::strcmp(m_list[i].getName(), dinosaur_name) == 0){
-            return &m_list[i];
+int Cage::findIndex(const char* dinosaur_name) const {
+    for(unsigned i = 0; i < m_list.size(); i++){
+        if(m_list[i].getName() == dinosaur_name){
+            return i;
         }
     }
-    return nullptr;
+    return -1;
 }
 
 bool Cage::serialize(std::ofstream &ofs) const {
-    if(!MySpace::serialize(ofs, m_climate)){
+    if(!m_climate.serialize(ofs)){
         return false;
     }
-    if(!MySpace::serialize(ofs, m_era)){
+    if(!m_era.serialize(ofs)){
         return false;
     }
-    ofs.write((char const*) &m_size, sizeof(m_size));
-    ofs.write((char const*) &m_list_count, sizeof(m_list_count));
-    for(unsigned i = 0; i < m_list_count; i++){
+    unsigned size = m_list.size();
+    ofs.write((char const*) &size, sizeof(size));
+    size = m_list.capacity();
+    ofs.write((char const*) &size, sizeof(size));
+    for(unsigned i = 0; i < m_list.size(); i++){
         if(!m_list[i].serialize(ofs)){
             break;
         }
@@ -115,19 +81,17 @@ bool Cage::serialize(std::ofstream &ofs) const {
 }
 
 bool Cage::unserialize(std::ifstream &ifs) {
-    if(!MySpace::unserialize(ifs, m_climate)){
+    if(!m_climate.unserialize(ifs)){
         return false;
     }
-    if(!MySpace::unserialize(ifs, m_era)){
+    if(!m_era.unserialize(ifs)){
         return false;
     }
-    ifs.read((char*) &m_size, sizeof(m_size));
-    ifs.read((char*) &m_list_count, sizeof(m_list_count));
-    if(m_list != nullptr){
-        delete[] m_list;// I'm sure that this won't be needed; (never used)
-    }
-    m_list = new Dinosaur[m_size];
-    for(unsigned i = 0; i < m_list_count; i++){
+    unsigned size, capacity;
+    ifs.read((char*) &size, sizeof(size));
+    ifs.read((char*) &capacity, sizeof(capacity));
+    m_list = Vector<Dinosaur>(capacity, false);
+    for(unsigned i = 0; i < size; i++){
         if(!m_list[i].unserialize(ifs)){
             break;
         }
@@ -135,56 +99,11 @@ bool Cage::unserialize(std::ifstream &ifs) {
     return !(!ifs);
 }
 
-void Cage::clear(){
-    if(m_climate != nullptr){
-        delete[] m_climate;
-        m_climate = nullptr;
-    }
-    if(m_era != nullptr){
-        delete[] m_era;
-        m_era = nullptr;
-    }
-    if(m_list != nullptr){
-        delete[] m_list;
-        m_list = nullptr;
-    }
-}
-
-void Cage::copy(const Cage &other){
-    copy(other.m_climate, other.m_size, other.m_era, other.m_list, other.m_list_count);
-}
-
-void Cage::copy(char const* climate, const unsigned size, char const* era,
-                const Dinosaur* list, const unsigned list_count){
-    //clear();
-    m_size = size;
-    MySpace::mem_copy(m_climate, climate);
-    MySpace::mem_copy(m_era, era);
-    if(m_list != nullptr){
-        delete[] m_list;
-        m_list = nullptr;
-    }
-    if(size == Cage::DEFAULT_CAGE_SIZE){
-        // can be from the default constructor (the default value of func getCageSize() is the same)
-        return;// it's safe
-    }
-    m_list = new Dinosaur[size];
-    m_list_count = list_count;
-    if(list != nullptr){
-        for(unsigned i = 0; i < list_count; i++){
-            m_list[i] = list[i];
-        }
-    }
-}
-
 std::ostream& operator<<(std::ostream& out, Cage const& obj){
-    out << "Climate: " << (obj.m_climate == nullptr ? "<no-info>" : obj.m_climate) << std::endl;
-    out << "Era: " << (obj.m_climate == nullptr ? "no era" : obj.m_climate) << std::endl;
-    out << "Has: " << obj.m_list_count << "/" << obj.m_size << std::endl;
-    if(obj.m_list == nullptr){
-        return out << "Empty cage." << std::endl;
-    }
-    for(unsigned i = 0; i < obj.m_list_count; i++){
+    out << "Climate: " << (obj.m_climate.empty() ? "<no-info>" : obj.m_climate) << std::endl;
+    out << "Era: " << (obj.m_era.empty() ? "no era" : obj.m_era) << std::endl;
+    out << "Has: " << obj.m_list.size() << "/" << obj.m_list.capacity() << std::endl;
+    for(unsigned i = 0; i < obj.m_list.size(); i++){
         out << obj.m_list[i] << std::endl;
     }
     return out;
--- Cage.hpp
+++ Cage.hpp
@@ -5,58 +5,32 @@
 #ifndef JURASSICPARK_CAGE_HPP
 #define JURASSICPARK_CAGE_HPP
 
-#include "Dinosaur.hpp"
 #include <fstream>
 //for: ifstream, ofstream
 #include <iostream>
 //for: ostream, endl
-
-/*enum class CageClimate {
-    UNKNOWN = -1,
-    MEDITERRANEAN,
-    SUBTROPICAL,
-    OCEANIC,
-    CONTINENTAL
-};
-
-enum class CageSize {
-    SMALL, // size for 1
-    MEDIUM, // size for 3
-    LARGE // size for 10
-};*/
+#include "Dinosaur.hpp"
+#include "String.hpp"
+#include "Vector.hpp"
 
 class Cage {
-    char* m_climate;
-    unsigned m_size;// list limit
-    char* m_era;// if any dinosaur; all dinosaurs MUST be (and are) from one era
-    Dinosaur* m_list;
-    unsigned m_list_count;// list count
+    String m_climate;
+    String m_era;// if any dinosaur; all dinosaurs MUST be (and are) from one era
+    Vector<Dinosaur> m_list;
 public:
-    static unsigned getCageSize(char const* size);
+    static unsigned getCageSize(String const& size);
     static const unsigned DEFAULT_CAGE_SIZE = 0;// MUST be different from the other cage sizes
 
-    Cage(char const* climate = nullptr, char const* size = nullptr);
-    Cage(const Cage& other);
-    Cage& operator=(const Cage& other);
-    ~Cage();
-
-    bool empty() const;// true if empty
-    bool full() const;// true if full
-    unsigned free() const;// returns the free left space
+    Cage(String const& climate = nullptr, String const& size = nullptr);
 
     bool canHandle(const Dinosaur& dinosaur);
     bool add(const Dinosaur& dinosaur);
     bool remove(const char* dinosaur_name);
-    Dinosaur* find(const char* dinosaur_name) const;
+    int findIndex(const char* dinosaur_name) const;
     bool serialize(std::ofstream& ofs) const;
     bool unserialize(std::ifstream& ifs);
 
     friend std::ostream& operator<<(std::ostream& out, Cage const& obj);
-private:
-    void clear();
-    void copy(const Cage &other);
-    void copy(char const* climate, const unsigned size, char const* era,
-            const Dinosaur* list, const unsigned list_count);
 };
 
 std::ostream& operator<<(std::ostream& out, Cage const& obj);
