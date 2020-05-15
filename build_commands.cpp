f98130d38ef38c90d64b2b85046f7fdfc574bf6a
Veselin Ivanov <ivanov1234159@gmail.com>
2020-05-15 21:56:01 +0300

    link (use) commands CmdFeedAnimals and CmdSupplyFood

--- build_commands.cpp
+++ build_commands.cpp
@@ -6,15 +6,20 @@
 #include "CmdAddAnimal.hpp"
 #include "CmdCreateCage.hpp"
 #include "CmdExit.hpp"
+#include "CmdFeedAnimals.hpp"
 #include "CmdHelp.hpp"
 #include "CmdSave.hpp"
 #include "CmdStatus.hpp"
+#include "CmdSupplyFood.hpp"
 
 void build_commands(){
     Commander::add(new CmdAddAnimal);
     Commander::add(new CmdCreateCage);
     Commander::add(new CmdExit);
+    Commander::add(new CmdFeedAnimals);
     Commander::add(new CmdHelp);
     Commander::add(new CmdSave);
     Commander::add(new CmdStatus);
+    Commander::add(new CmdSupplyFood);
+    //TODO: add some people (to work in the zoopark)
 }
