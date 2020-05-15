6adaf03327a29fe5c6829d1830c0c89dd695b060
Veselin Ivanov <ivanov1234159@gmail.com>
2020-05-15 21:59:49 +0300

    use class String to remove the need of Commander::BUFFER_SIZE in class CmdAddAnimal; include <iostream> in CmdAddAnimal

--- cmd/CmdAddAnimal.cpp
+++ cmd/CmdAddAnimal.cpp
@@ -2,15 +2,18 @@
 // Created by toi on 07/05/2020.
 //
 
+#include <iostream>
+//for: cout, endl
 #include "CmdAddAnimal.hpp"
 #include "../Commander.hpp"
 #include "../Dinosaur.hpp"
+#include "../String.hpp"
 
 CmdAddAnimal::CmdAddAnimal(): Command("add", "<name> <gender> <era> <type> <species> <food>",
         "add a new dinosaur in the zoopark") {}
 
 bool CmdAddAnimal::action(RunnerType &runner, std::istringstream& iss) const {
-    char buff[Commander::BUFFER_SIZE];
+    String buff;
     iss >> buff;
     if(!iss){
         std::cout << "Some error. Cannot read the name." << std::endl;//TODO: fix message
@@ -42,6 +45,11 @@ bool CmdAddAnimal::action(RunnerType &runner, std::istringstream& iss) const {
     if(!iss || !dino.setFood(buff)){
         std::cout << "Invalid food name." << std::endl;
         return false;
+    }
+    dino.setFoodAmount(1);//TODO: its better to be read from the console
+    if(runner.hasDinosaur(dino.getName())){
+        std::cout << "Duplicate name. Please, change the name of your dinosaur." << std::endl;
+        return true;
     }
     if(!runner.addAnimal(dino)){
         std::cout << "No cage for this dinosaur. Please create one." << std::endl;
